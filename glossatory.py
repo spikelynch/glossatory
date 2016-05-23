2948211bc2b3d8769b6eaa524b1475249fe86293
Mike Lynch <mike.lynch@iinet.net.au>
2016-05-23 07:59:35 +0000

    glossatory can now be configured to sleep for a random number of seconds before posting

--- glossatory.py
+++ glossatory.py
@@ -2,6 +2,7 @@
 
 from twitterbot import TwitterBot
 import torchrnn
+import time, random
 
 
 class Glossatory(TwitterBot):
@@ -9,12 +10,20 @@ class Glossatory(TwitterBot):
     def glossolalia(self):
         lines = torchrnn.generate_lines(temperature=self.cf['temperature'], model=self.cf['model'])
         return lines[0]
+
+    def random_pause(self):
+        if 'pause' in self.cf:
+            pause = random.randrange(0, int(self.cf['pause']))
+            print("Waiting for {}".format(pause))
+            time.sleep(pause)
+
         
         
 if __name__ == '__main__':
     g = Glossatory()
     g.configure()
     tweet = g.glossolalia()
+    g.random_pause()
     if tweet:
         g.post(tweet)
     else:
