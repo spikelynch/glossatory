a4e5c6e487cb3440ae66c1c2cc083afadb0e5e2e
Mike Lynch <mike.lynch@iinet.net.au>
2016-06-14 09:16:21 +0000

    Added regexp filter to glossatory

--- glossatory.py
+++ glossatory.py
@@ -2,7 +2,7 @@
 
 from twitterbot import TwitterBot
 import torchrnn
-import time, random, os.path
+import time, random, os.path, re
 
 
 class Glossatory(TwitterBot):
@@ -11,13 +11,21 @@ class Glossatory(TwitterBot):
     # note - filter lines to make sure that they include a definition
     # before doing the random choice.
     def glossolalia(self):
-        lines = torchrnn.generate_lines(n=self.cf['sample'], temperature=self.cf['temperature'], model=self.cf['model'])
-        log = os.path.join(self.cf['logs'], str(time.time())) + '.log'
-        with open(log, 'wt') as f:
-                for line in lines:
-                        f.write(line)
-                        f.write("\n")
-        return random.choice(lines[2:-1])
+        result = None
+        loop = 10
+        accept_re = re.compile(self.cf['accept'])
+        while not result and loop > 0:
+                lines = torchrnn.generate_lines(n=self.cf['sample'], temperature=self.cf['temperature'], model=self.cf['model'])
+                lines = [ l for l in lines if accept_re.match(l) ]
+                log = os.path.join(self.cf['logs'], str(time.time())) + '.log'
+                with open(log, 'wt') as f:
+                        for line in lines:
+                                f.write(line)
+                                f.write("\n")
+                if len(lines) > 5:
+                    result = random.choice(lines[2:-2])
+                loop = loop - 1
+        return result
 
     def random_pause(self):
         if 'pause' in self.cf:
--- glossatory.sh
+++ glossatory.sh
@@ -5,4 +5,4 @@ ghome="/home/pi/bots/glossatory"
 
 export PYTHONPATH="/home/pi/bots/twitterbot:$PYTHONPATH"
 
-/usr/bin/python3.2 ${ghome}/glossatory.py  -c ${ghome}/config.yml
+/usr/bin/python3.2 ${ghome}/glossatory.py -c ${ghome}/config.yml
