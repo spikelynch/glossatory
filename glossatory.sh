febcca8d16b97130dda39d274f8912e92ea1f622
Mike Lynch <mike.lynch@iinet.net.au>
2017-04-14 14:52:04 +1000

    New generic version of the bot client

--- glossatory.sh
+++ glossatory.sh
@@ -4,5 +4,7 @@
 ghome="/home/pi/bots/glossatory"
 
 export PYTHONPATH="/home/pi/bots/twitterbot:$PYTHONPATH"
+export TORCH_RNN="/Users/mike/torch/torch-rnn"
+export TORCH_TH="/Users/mike/torch/install/bin/th"
 
 /usr/bin/python3.2 ${ghome}/glossatory.py -c ${ghome}/config.yml
