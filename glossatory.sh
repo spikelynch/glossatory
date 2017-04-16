8e160d9a74deca6aa076b5649ea528e9692048cc
Mike Lynch <mike.lynch@iinet.net.au>
2017-04-16 22:09:24 +0000

    Added torch paths

--- glossatory.sh
+++ glossatory.sh
@@ -4,5 +4,7 @@
 ghome="/home/pi/bots/glossatory"
 
 export PYTHONPATH="/home/pi/bots/twitterbot:$PYTHONPATH"
+export TORCH_RNN="/home/pi/torch/torch-rnn"
+export TORCH_TH="/home/pi/torch/install/bin/th"
 
 /usr/bin/python3.2 ${ghome}/glossatory.py -c ${ghome}/config.yml
