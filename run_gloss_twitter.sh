a5fefaaa85203ec2ead3eaa8c74b2835720bdbf7
Mike Lynch <mike.lynch@iinet.net.au>
2017-04-19 09:06:03 +0000

    Committing local changes before update

--- run_gloss_twitter.sh
+++ run_gloss_twitter.sh
@@ -7,4 +7,4 @@ export PYTHONPATH="/home/pi/bots/botclient:$PYTHONPATH"
 export TORCH_RNN="/home/pi/torch/torch-rnn"
 export TORCH_TH="/home/pi/torch/install/bin/th"
 
-/usr/bin/python3.2 ${ghome}/glossatory.py -d  -c ${ghome}/mastodon_conf.yml
+/usr/bin/python3.2 ${ghome}/glossatory.py -s Twitter -c ${ghome}/twitter_conf.yml
