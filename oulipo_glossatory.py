3f9d2b1d5268bc89a895f2ce6ef64ea771aaa5a3
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-02-20 08:06:10 +1100

    Local changes

--- oulipo_glossatory.py
+++ oulipo_glossatory.py
@@ -28,8 +28,8 @@ if __name__ == '__main__':
         og.colon = DEFAULT_COLON
     defn = og.get_next()
     if defn:
-    	og.random_pause()
-    	options = {}
+        og.random_pause()
+        options = {}
         if 'content_warning' in og.cf:
             options['spoiler_text'] = og.cf['content_warning'].format(defn[0])
         og.post(defn[0] + og.colon + defn[1], options)
